b5eeaec953a6ca7a17d73d231d2c5a0599d239fd
Alan Gonzalez <161752157+AlanGF1@users.noreply.github.com>
2024-04-26 12:16:46 -0600

    Add files via upload

---
+++ carrito-react-mar/src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container"></div>
+        <h1>Marvel</h1>
+        <p>El mejor lugar para comprar Libros</p>
+    </div>
+  );
+}
+export default Banner;
